391c67464e2f660a9e87ca1d68a577cdab11b16a
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-05-14 15:31:35 +0200

    Keyboard EventListener Functionality

--- index.js
+++ index.js
@@ -33,3 +33,44 @@ for (let i = 0; i < numberOfButtons; i++)
     })
     
 }
+document.addEventListener("keydown", function (event){
+
+    switch(event.key)
+    {
+        case 'w':
+            audio  = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+
+        case 'a':
+            audio  = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+
+        case 's':
+            audio  = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+
+        case 'd':
+            audio  = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+
+        case 'j':
+            audio  = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+
+        case 'k':
+            audio  = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+
+        case 'l':
+            audio  = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        default:
+    }
+})
